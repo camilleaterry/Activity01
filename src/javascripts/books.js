80b9dd6bbb23fded9d609861f6af1224a28d443b
Camille <camilleaterry@gmail.com>
2022-01-18 23:30:14 -0700

    Activity01
    favorite ten books

---
+++ src/javascripts/books.js
@@ -0,0 +1,83 @@
+export const books = [
+    {
+      "id": 102001,
+      "title": "The Great Gatsby",
+      "author": "F. Scott Fitzgerald",
+      "description": "The novel chronicles an era that Fitzgerald himself dubbed the 'Jazz Age'. Following the shock and chaos of World War I, American society enjoyed unprecedented levels of prosperity during the 'roaring twenties' ",
+      "genre": "Crime, Drama",
+      "published": 1925
+    },
+    {
+  
+      "id": 102002,
+      "title": "The Godfather",
+      "author": "Herman Melville",
+      "description": "First published in 1851, Melville's masterpiece is, in Elizabeth Hardwick's words, 'the greatest novel in American literature.' The saga of Captain Ahab and his monomaniacal pursuit of the white whale remains a peerless adventure story but one full of mythic grandeur, poetic majesty, and symbolic power.  ",
+      "genre": "Crime, novalf",
+      "published": 1969
+    },
+    {
+      "id": 102003,
+      "title": "Moby Dick",
+      "author": "Mario Puzo",
+      "description": "The novel details the story of a fictional Mafia family in New York City (and Long Island), headed by Vito Corleone, the Godfather. The novel covers the years 1945 to 1955 and includes the back story of Vito Corleone from early childhood to adulthood. ",
+      "genre": "novalf",
+      "published": 1851
+    },
+    {
+      "id": 102004,
+      "title": "Pride and Prejudice ",
+      "author": "Jane Austen",
+      "description": "The book is narrated in free indirect speech following the main character Elizabeth Bennet as she deals with matters of upbringing, marriage, moral rightness and education in her aristocratic society. ",
+      "genre": "Romance novel",
+      "published": 1813
+    },
+    {
+      "id": 102005,
+      "title": "The Adventures of Huckleberry Finn",
+      "author": "Mark Twain",
+      "description": "Revered by all of the town's children and dreaded by all of its mothers, Huckleberry Finn is indisputably the most appealing child-hero in American literature. ",
+      "genre": "Picaresque novel",
+      "published": 1884
+    },
+    {
+      "id": 102006,
+      "title": "A Brief History of Time",
+      "author": "Stephen Hawking",
+      "description": "Most science books, even well-written ones, read a bit too much like a textbook, but renowned English physicist, cosmologist, and author Stephen Hawking manages to turn some of the world’s most profound questions—How did the universe begin? What happens in the end?",
+      "genre": "Popular science",
+      "published": 1988
+    },
+    {
+      "id": 102007,
+      "title": "A Wrinkle in Time",
+      "author": "Madeleine L’Engle",
+      "description": "While this book is getting a fresh look because of the film starring Oprah, Mindy Kaling, and others, Madeleine L’Engle’s A Wrinkle in Time has long been held as a must-read for its fantastical telling of splitting the fabric of time and space. ",
+      "genre": "Novel",
+      "published": 1962
+    },
+    {
+      "id": 102008,
+      "title": "Alice’s Adventures in Wonderland & Through the Looking-Glass",
+      "author": "Lewis Carrol",
+      "description": "If all you know of Lewis Carrol’s Wonderland is the zany but sanitized version in the 1951 Walt Disney animation, it’s time to flip your perspective on its head—much like the Cheshire Cat might flip himself. ",
+      "genre": "Children's fiction",
+      "published": 1871
+    },
+    {
+      "id": 102009,
+      "title": "Beloved",
+      "author": "Toni Morrison",
+      "description": "Toni Morrison’s Beloved stares down the horrors of slavery and transforms a narrative you think you’ve read a hundred times into a towering story of pain, agony, triumph, and freedom.",
+      "genre": "American Literature",
+      "published": 1987
+    },
+    {
+      "id": 102010,
+      "title": "Charlotte’s Web",
+      "author": "E.B. White",
+      "description": "You’re never too old to visit with Charlotte, Wilbur, and Templeton. This heart-warming tale of friendship and dedication follows young Wilbur, a runt of a pig, as he’s spared from one death but subsequently sent to another almost-certain death.",
+      "genre": "Children's literature",
+      "published": 1952
+    }
+  ]
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -9,36 +9,54 @@ import "bootstrap"
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { FaReact, FaNode, FaDatabase, FaShippingFast } from 'react-icons/fa'
+import {books} from './books'
+import { main } from "@popperjs/core";
 
-// Example class component
-export class PoweredBy extends React.Component {
+class Main extends React.Component {
+  render()  {
+    return <BookList books= {books} />
+  }
+}
+
+class BookList extends React.Component {
   render(){
     return (
-      <h2 className="lead text-muted my-3">Powered by {this.props.framework}</h2>
+      <div className="container">
+        <header>
+          <h1>Top 10 Books</h1>
+        </header>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>List</li>
+            <li>About</li>
+          </ul>
+        </nav>
+        <main>
+          {this.props.books.map(b => {
+            return <Book key={b.id} book={b}/>
+          })}
+        </main>
+      </div>
     )
   }
 }
 
-// Example function components
-export function MERNDisplay(props) {
+function Book(props){
+  const b = props.book
   return (
-    <div className="course-icons">
-      <FaNode size="30%" color="#43853d" className="mt-5"/><br/>
-      <FaReact size="20%" color="skyblue" className="m-3" />
-      <FaShippingFast size="20%" color="#259dff" className="m-3"/>
-      <FaDatabase size="20%" color="#00ED64" className="m-3"/>
+    <div className="card"> 
+      <h2>{b.title}</h2>
+      <h3>{b.author}</h3>
+      <p>{b.description}</p>
+      <ul className="extra">
+        <li><strong>{b.genre}</strong> </li>
+        <li><strong>{b.published}</strong> </li>
+        <li><button className="primary">Select</button></li>
+      </ul>
     </div>
+
   )
 }
 
-export function Welcome(props) {
-  return (
-    <div className="container my-5 py-5 text-center">
-      <MERNDisplay/>
-      <h1 className="mt-3 display-5">Welcome to {props.to}</h1>
-      <PoweredBy framework={`React from ${props.using}`}/>
-    </div>
-  )
-}
-
-ReactDOM.render(<Welcome to="WEB 3430" using="the MERN stack"/>, document.getElementById('main'))
+ReactDOM.render(<Main/>, document.getElementById('main'))
